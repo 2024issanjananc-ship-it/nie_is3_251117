c951438017a918ac95f5aee5607a419f8eb0ff72
2024issanjananc-ship-it <2024is_sanjanan_c@nie.ac.in>
2025-11-18 11:30:03 +0530

    square

---
+++ squareCAlc.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  const [calcData, setCalcData] = useState({num: 20.0, result: null});
+  const doCalc = () =>{
+    if(isNaN(calcData.num)){
+      setCalcData({...calcData, result : 'Invalid num'})
+      return;
+    }
+    const num = calcData.num;
+    const sqr = num * num;
+    setCalcData({...calcData, result:`Result of ${num} is ${sqr}`})
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Square Calculator Application</Text>
+      <TextInput value={calcData.num} keyboardtype='numeric'
+      placeholder='enter number'
+      onchangeText={(data) => setCalcData({...calcData, num: data})}/>
+      <Button title='Find Square' onPress={doCalc}/>
+      <Text>{calcData.result}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },title :{
+    fontSize :22,
+    fontWeight :'bold',
+  },
+});
